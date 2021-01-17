ca8c3dd16a8f658885f89c03758e14d48bfa6c8d
kunkunzhu <zkzhu@uwaterloo.ca>
2021-01-17 01:02:31 -0500

    color change

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,7 @@ function Home() {
 
     return (
         <>
-            <h1 class="title">ENERGi</h1>
+            <h1 class="title" id="icon">ENERGi</h1>
             <Graph data={[65, 59, 80, 81, 56, window.localStorage.getItem("recent")]} name="today's progress" />
             <TabBar />
         </>
